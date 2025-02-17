04716a12f46aebcd985d0e654a7289cfb7c7a81f
shadcn <m@shadcn.com>
2025-02-17 11:43:31 +0400

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <p className="text-2xl font-bold">Hello World</p>
+    </div>
+  )
+}
